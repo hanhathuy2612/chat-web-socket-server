4a1d5764bcd67821a0f116d6ce51d1a875452359
huyhn <huy.ha@formos.com>
2023-06-13 14:39:13 +0700

    init

---
+++ src/main/java/com/huyhn/chat_web_socket_server/ChatWebSocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.huyhn.chat_web_socket_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWebSocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatWebSocketServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/huyhn/chat_web_socket_server/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.huyhn.chat_web_socket_server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/chat").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/chat2").setAllowedOrigins("*");
+    }
+}
