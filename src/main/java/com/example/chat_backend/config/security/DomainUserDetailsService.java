944b5c56c63c05d02fda69a3f7a305467f2d9191
huyha <withuwe021@gmail.com>
2025-01-02 19:22:42 +0700

    refactor: improve user authentication logging and simplify UserDetails creation

--- src/main/java/com/example/chat_backend/config/security/DomainUserDetailsService.java
+++ src/main/java/com/example/chat_backend/config/security/DomainUserDetailsService.java
@@ -1,21 +1,22 @@
 package com.example.chat_backend.config.security;
 
-import com.example.chat_backend.domain.AppUser;
-import com.example.chat_backend.domain.Authority;
-import com.example.chat_backend.repository.AppUserRepository;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.validator.internal.constraintvalidators.hv.EmailValidator;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.Locale;
+import com.example.chat_backend.domain.AppUser;
+import com.example.chat_backend.domain.Authority;
+import com.example.chat_backend.repository.AppUserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * Authenticate a user from the database.
@@ -32,31 +33,24 @@ public class DomainUserDetailsService implements UserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(final String login) {
-        log.debug("Authenticating {}", login);
-
-//        if (new EmailValidator().isValid(login, null)) {
-//            return userRepository
-//                .findOneWithAuthoritiesByEmailIgnoreCase(login)
-//                .map(user -> createSpringSecurityUser(login, user))
-//                .orElseThrow(() -> new UsernameNotFoundException("User with email " + login + " was not found in the database"));
-//        }
+        log.info("Authenticating {}", login);
 
         return userRepository
-            .findOneWithAuthoritiesByLogin(login)
-            .map(user -> createSpringSecurityUser(login, user))
-            .orElseThrow(() -> new UsernameNotFoundException("User " + login + " was not found in the database"));
+                .findOneWithAuthoritiesByLogin(login)
+                .map(user -> createSpringSecurityUser(login, user))
+                .orElseThrow(() -> new UsernameNotFoundException("User " + login + " was not found in the database"));
     }
 
-    private org.springframework.security.core.userdetails.User createSpringSecurityUser(String lowercaseLogin, AppUser user) {
+    private User createSpringSecurityUser(String lowercaseLogin, AppUser user) {
         if (!user.isActivated()) {
             throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated");
         }
         List<SimpleGrantedAuthority> grantedAuthorities = user
-            .getAuthorities()
-            .stream()
-            .map(Authority::getName)
-            .map(SimpleGrantedAuthority::new)
-            .toList();
-        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(), grantedAuthorities);
+                .getAuthorities()
+                .stream()
+                .map(Authority::getName)
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+        return new User(user.getLogin(), user.getPassword(), grantedAuthorities);
     }
 }
