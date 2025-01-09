bc7570e33164b96998c7a6d5272a0d23f325e08f
huyha <withuwe021@gmail.com>
2025-01-09 23:16:21 +0700

    refactor: clean up imports in SecurityUtils.java by removing unused statements

--- src/main/java/com/example/chat_backend/config/security/SecurityUtils.java
+++ src/main/java/com/example/chat_backend/config/security/SecurityUtils.java
@@ -1,5 +1,9 @@
 package com.example.chat_backend.config.security;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
@@ -7,10 +11,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
-
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 /**
  * Utility class for Spring Security.
